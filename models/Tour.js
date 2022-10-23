c7da7e030cd7fd6017dc0bc6af48e84dce9b509b
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-10-23 11:39:22 +0600

    installation setup

---
+++ models/Tour.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+// Schema Design
+const tourSchema=mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for this tour"],
+        trim: true,
+        unique: [true, "Name must be unique"],
+        minLength: [3, "Name must be at least 3 characters."],
+        maxLength: [100, "Name is too long."]
+      },
+      image: {
+        type: String,
+        required: [true, "Please provide a image for this tour"],
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      cost: {
+        type: Number,
+        required: [true, "Please provide a price for this tour"],
+        min: [0, "Cost can't be negative"],
+      },
+      catagory: {
+        type: String,
+        required: [true, "Please provide a catagory for this tour"],
+      },
+},{ timestamps: true })
